80754ffa1d5440408ffef0d164023ce247f39dc1
SeungYun Kim <70507109+yuuniee@users.noreply.github.com>
2024-08-25 17:55:52 +0900

    Add files via upload

--- navigation/dashboard_ti2.py
+++ navigation/dashboard_ti2.py
@@ -16,6 +16,8 @@ MA_COLOR3 = 'purple'
 
 PC_COLOR = 'grey'
 
+STG_LIST = [['RSI', 14, 'blue'], ['AROON', 14, 'green'], ['MFI', 14, 'white'], ['WILLR', 14, 'violet'],]# ['HT_DCPERIOD', 0, 'lime']]
+
 
 def normalize(df):
     # return (df - df.min())/(df.max() - df.min())
@@ -23,12 +25,52 @@ def normalize(df):
     # return np.log(df)
 
 
+def style_dataframe(df):
+    return df.style.set_table_styles(
+        [{
+            'selector': 'th',
+            'props': [
+                ('background-color', '#4CAF50'),
+                ('color', 'white'),
+                ('font-family', 'Arial, sans-serif'),
+                ('font-size', '16px')
+            ]
+        },
+        {
+            'selector': 'td, th',
+            'props': [
+                ('border', '2px solid #4CAF50')
+            ]
+        }]
+    )
+
+
+# style
+th_props = [
+    ('font-size', '14px'),
+    ('text-align', 'center'),
+    ('font-weight', 'bold'),
+    ('color', '#FF0000'),
+    ('background-color', '#f7ffff')
+]
+
+td_props = [
+    ('font-size', '16px'),
+    ('color', '#FF0000'),
+]
+
+styles = [
+    dict(selector="th", props=th_props),
+    dict(selector="td", props=td_props)
+]
+
+
 def get_indicator(df, name='MA', p1=14, p2=14):
     if name == 'MA':
         res = talib.MA(df.Close, p1)
     elif name == 'RSI':
         res = talib.RSI(df.Close, p1)
-    elif name == 'AROONOSC':
+    elif name == 'AROON':
         res = (talib.AROONOSC(df.High, df.Low, p1) + 100) / 2
     elif name == 'MFI':
         res = talib.MFI(df.High, df.Low, df.Close, df.Volume, p1)
@@ -36,6 +78,8 @@ def get_indicator(df, name='MA', p1=14, p2=14):
         res = talib.WILLR(df.High, df.Low, df.Close, p1) + 100
     # elif name == 'STOCHRSI':
     #     res = talib.STOCHRSI()
+    # elif name == 'HT_DCPERIOD':
+    #     res = talib.HT_DCPERIOD(df.Close)
     else:
         res = talib.MA(df.Close, p1)
 
@@ -97,82 +141,124 @@ def page3() -> None:
     st.title(f'Technical Indicators', help='Indicator values are scaled as 0 to 100')#divider='gray')
     st.divider()
 
+    # backtest
+    temp_df = coin_df[-period_dict[check]:].copy()
+    temp_df['Date'] = temp_df['Date'].astype(str).str.split(' ').str[0]
+    temp_df['Date'] = pd.to_datetime(temp_df['Date'])
+    temp_df = temp_df.set_index('Date')
+
+    res = list()
+    params = list()
+    for id in STG_LIST:
+        if id[0] == 'RSI':
+            sg = backtest.RelativeStrengthIndexStrategy
+        elif id[0] == 'MFI':
+            sg = backtest.MoneyFlowIndexStrategy
+        elif id[0] == 'AROON':
+            sg = backtest.AroonOSCStrategy
+        elif id[0] == 'WILLR':
+            sg = backtest.WilliamsPercentR
+        else:
+            # res.append(backtest.run_test(data=temp_df, sg=sg))
+            # params.append((sg.n_enter, sg.n_exit))
+            continue
+        id[1] = sg.n1
+        res.append(backtest.run_test(data=temp_df, sg=sg))
+        params.append((sg.n_enter, sg.n_exit))
+
     # Call Indicators
     ma_list = [('MA', 20, MA_COLOR), ('MA', 60, MA_COLOR2), ]
     for ma in ma_list:
         coin_df[f'{ma[0]}{ma[1]}'] = get_indicator(coin_df, name=ma[0], p1=ma[1], )
     # variance = round(np.var(coin_df['Close']),3)
-    id_list = [('RSI', 14, 'blue'), ('AROONOSC', 14, 'green'), ('MFI', 14, 'orange'), ('WILLR', 14, 'violet'), ]
-    for id in id_list:
+
+    for id in STG_LIST:
         coin_df[f'{id[0]}{id[1]}'] = get_indicator(coin_df, name=id[0], p1=id[1], )
+
     coin_df = coin_df[-period_dict[check]:]
     coin_df['50'] = 50
-
-    # backtest
-    temp_df = coin_df.copy()
-    temp_df['Date'] = temp_df['Date'].astype(str).str.split(' ').str[0]
-    temp_df['Date'] = pd.to_datetime(temp_df['Date'])
-    temp_df = temp_df.set_index('Date')
-
-    # res = backtest.run_test(data=temp_df, st=backtest.RelativeStrengthIndexStrategy)
-    # res2 = backtest.run_test(data=temp_df, st=backtest.MoneyFlowIndexStrategy)
-
-    res_names = list()
-    res = list()
-    for id in id_list:
-        if id[0] == 'RSI':
-            sg = backtest.RelativeStrengthIndexStrategy
-        elif id[0] == 'MFI':
-            sg = backtest.MoneyFlowIndexStrategy
-        elif id[0] == 'AROONOSC':
-            sg = backtest.AroonOSCStrategy
-        elif id[0] == 'WILLR':
-            sg = backtest.WilliamsPercentR
-        else:
-            continue
-        res_names.append(id[0])
-        res.append(backtest.run_test(data=temp_df, sg=sg))
-
     df = pd.DataFrame(
         {
-            "name": [n for n in res_names],
-            "rate": [n['rate'] for n in res],
-            "return": [n['return'] for n in res],
-            "MDD(%)": [n['MDD'] for n in res],
-            "sharpe": [n['sharpe'] for n in res],
+            "name": [f'{n[0]} {n[1]}' for n in STG_LIST],
+            "Long Enter": [n[0] for n in params],
+            "Long Exit": [n[1] for n in params],
+            "rate": [round(n['rate'],2) for n in res],
+            "return": [round(n['return'],2) for n in res],
+            "MDD(%)": [round(n['MDD'], 2) for n in res],
+            "sharpe": [round(n['sharpe'], 2) for n in res],
             "trades": [n['trades'] for n in res],
             "PnL": [n['PnL'].values[:] for n in res],
         }
     )
-    df = df.style.set_properties(**{'font-size': '22pt', 'font-family': 'Calibri'})
-    st.dataframe(
-        df,
-        column_config={
-            "name": st.column_config.Column("Strategy"),
-            "rate": st.column_config.NumberColumn(
-                "🎯 Win Rate(%)",
-                help="Win Rate",
-                format="%d ",
-            ),
-            "MDD(%)": st.column_config.NumberColumn("💣 MDD(%)"),
-            "return": st.column_config.NumberColumn("💰 Return(%)"),
-            "sharpe": st.column_config.NumberColumn("🔪 Sharpe Ratio"),
-            "trades": st.column_config.NumberColumn("🎮 Trade Count"),
-            "PnL": st.column_config.LineChartColumn(
-                "📈 PnL", y_min=70, y_max=150
-            ),
-        },
-        hide_index=True,
-        # height=800,
-        # width=1500,
-        use_container_width=True
+
+    def trade_reco(d, enter, exit, ):
+
+        return
+    df2 = pd.DataFrame(
+        {
+            "name": [f'{n[0]} {n[1]}' for n in STG_LIST],
+            "Status": ['Strong Buy' if n[1] >= coin_df[f'{n[0]}{n[1]}'].iloc[-1] else 'Neutral' for n in STG_LIST],
+            "Current Value": [coin_df[f'{n[0]}{n[1]}'].iloc[-1] for n in STG_LIST],
+            # "Long Enter": [n[0] for n in params],
+            # "Long Exit": [n[1] for n in params],
+        }
     )
+    # df = df.style.set_properties(**{'font-size': '22pt', 'font-family': 'Calibri'})
+    # df2 = df2.style.set_properties(**{'font-size': '22pt', 'font-family': 'Calibri'})
+    # df2 = style_dataframe(df2)
+    # df2 = df2.style.set_properties(**{'text-align': 'right'}).set_table_styles(styles)
+    def left_align(s, props='text-align: right;'):
+        return props
 
+    df = df.style.applymap(left_align)
+    # df2 = df2.style.applymap(left_align)
+    cm1, cm2 = st.columns([1, 1, ])
+    with cm1:
+        st.header('Current Status')
+        st.dataframe(
+            df2.style.applymap(left_align).format({"Current Value": "{:.2f}".format, }),
+            column_config={
+                "name": st.column_config.Column("Strategy"),
+                "Current Value": st.column_config.NumberColumn("Current Value"),
+                "Status": st.column_config.Column("Status"),
+                # "Long Enter": st.column_config.NumberColumn("📕 Long Enter"),
+                # "Long Exit": st.column_config.NumberColumn("📘 Long Exit"),
+            },
+            hide_index=True,
+            use_container_width=True,
+        )
+    with cm2:
+        st.header('Backtesting Record')
+        st.dataframe(
+            df.format({"rate": "{:.2f}".format, "return": "{:.2f}".format, "MDD(%)": "{:.2f}".format, "sharpe": "{:.2f}".format}),
+            column_config={
+                "name": st.column_config.Column("Strategy"),
+                "Long Enter": st.column_config.NumberColumn("📕 Long Enter"),
+                "Long Exit": st.column_config.NumberColumn("📘 Long Exit"),
+                "rate": st.column_config.NumberColumn(
+                    "🎯 Win Rate(%)",
+                    help="Win Rate",
+                    format="{:.2f}",
+                ),
+                "MDD(%)": st.column_config.NumberColumn("💣 MDD(%)"),
+                "return": st.column_config.NumberColumn("💰 Return(%)"),
+                "sharpe": st.column_config.NumberColumn("🔪 Sharpe Ratio"),
+                "trades": st.column_config.NumberColumn("🎮 Trade Count"),
+                "PnL": st.column_config.LineChartColumn(
+                    "📈 PnL", y_min=70, y_max=150
+                ),
+            },
+            hide_index=True,
+            # height=800,
+            # width=1500,
+            use_container_width=True,
+        )
 
+# with cm2:
     # Candle and volume chart
-    fig = make_subplots(rows=2+len(id_list), cols=1, shared_xaxes=True, vertical_spacing=0.025, horizontal_spacing=0.5,
-                        specs=[[{'secondary_y': True, }] for i in range(2+len(id_list))],
-                        row_heights=[100, 25] + [25 for i in range(len(id_list))]
+    fig = make_subplots(rows=2+len(STG_LIST), cols=1, shared_xaxes=True, vertical_spacing=0.025, horizontal_spacing=0.5,
+                        specs=[[{'secondary_y': True, }] for i in range(2+len(STG_LIST))],
+                        row_heights=[100, 25] + [25 for i in range(len(STG_LIST))]
                         )
     fig.add_trace(
         go.Candlestick(x=coin_df['Date'],
@@ -181,16 +267,16 @@ def page3() -> None:
                        name='BTC Candle',
                        ), row=1, col=1
     )
-    # In Momentum
-    for i, id in enumerate(id_list):
-        fig.add_trace(
-            go.Scatter(
-                x=coin_df['Date'],
-                y=coin_df[f'{id[0]}{id[1]}'],
-                line=dict(color=id[2], width=2, dash='dot'),
-                name=f'{id[0]}{id[1]}'
-            ), row=1, col=1, secondary_y=True
-        )
+    # # In Momentum
+    # for i, id in enumerate(STG_LIST):
+    #     fig.add_trace(
+    #         go.Scatter(
+    #             x=coin_df['Date'],
+    #             y=coin_df[f'{id[0]}{id[1]}'],
+    #             line=dict(color=id[2], width=2, dash='dot'),
+    #             name=f'{id[0]}{id[1]}'
+    #         ), row=1, col=1, secondary_y=True
+    #     )
     # In Moving Average
     for i, ma in enumerate(ma_list):
         fig.add_trace(
@@ -203,7 +289,7 @@ def page3() -> None:
         )
 
     # Out Momentum
-    for i, id in enumerate(id_list):
+    for i, id in enumerate(STG_LIST):
         fig.add_trace(
             go.Scatter(
                 x=coin_df['Date'],
@@ -224,7 +310,7 @@ def page3() -> None:
             y=coin_df['Volume'],
             marker=dict(color=coin_df['Volume'], colorscale='aggrnyl_r'),
             name='Volume'
-        ), row=2+len(id_list), col=1
+        ), row=2+len(STG_LIST), col=1
     )
     # fig['layout']['xaxis']['title'] = 'Date'
     fig['layout']['yaxis']['title'] = 'Price'
